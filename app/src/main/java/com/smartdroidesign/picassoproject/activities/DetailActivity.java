ebc189e6ce2fe4ca2af4359dbddf0c969ddf1fe9
Memnoc <smartdroidesign@gmail.com>
2018-02-11 13:14:19 +0000

    - implemented WallPaperManager class to set downloaded wallpapers as images

--- app/src/main/java/com/smartdroidesign/picassoproject/activities/DetailActivity.java
+++ app/src/main/java/com/smartdroidesign/picassoproject/activities/DetailActivity.java
@@ -1,13 +1,24 @@
 package com.smartdroidesign.picassoproject.activities;
 
+import android.app.WallpaperManager;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.smartdroidesign.picassoproject.R;
 import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 import static com.smartdroidesign.picassoproject.activities.MainActivity.EXTRA_CREATOR;
 import static com.smartdroidesign.picassoproject.activities.MainActivity.EXTRA_LIKES;
@@ -21,13 +32,14 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         Intent intent = getIntent();
-        String imageUrl = intent.getStringExtra(EXTRA_URL);
+        final String imageUrl = intent.getStringExtra(EXTRA_URL);
         String creatorName = intent.getStringExtra(EXTRA_CREATOR);
         int likeCount = intent.getIntExtra(EXTRA_LIKES, 0);
 
-        ImageView imageView = findViewById(R.id.image_view_detail);
+        final ImageView imageView = findViewById(R.id.image_view_detail);
         TextView TextViewCreator = findViewById(R.id.text_view_creator_detail);
         TextView TextViewLikes = findViewById(R.id.text_view_like_detail);
+        Button setWallBtn = findViewById(R.id.setWallpaperBtn);
 
         Picasso.with(this)
                 .load(imageUrl)
@@ -36,5 +48,25 @@ public class DetailActivity extends AppCompatActivity {
                 .into(imageView);
         TextViewCreator.setText(creatorName);
         TextViewLikes.setText("Likes :" + likeCount);
+
+        setWallBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("TAG", "CLICKED");
+                WallpaperManager wallManager = WallpaperManager.getInstance(getApplicationContext());
+                Bitmap bmpImg = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
+                try {
+                    wallManager.setBitmap(bmpImg);
+                    Toast.makeText(DetailActivity.this, "Wallpaper Set Successfully!!", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    Toast.makeText(DetailActivity.this, "Setting WallPaper Failed!!", Toast.LENGTH_SHORT).show();
+                }
+
+
+
+            }
+        });
+
     }
+
 }
